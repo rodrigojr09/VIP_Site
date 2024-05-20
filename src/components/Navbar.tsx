f0c14ae9b010c62eb67f9a674595871effd0b9e2
Rodrigo Junior <165495511+rodrigojr09@users.noreply.github.com>
2024-05-20 17:24:15 -0300

    feat *

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,24 @@
-export default function Navbar(){
-    return <div>
-        
+import Image from "next/image";
+import { FaSearch } from "react-icons/fa";
+
+export default function Navbar() {
+  return (
+    <div className="w-full min-w-screen p-2 text-black flex">
+        <div>
+            <Image src={"/Capa.png"} alt="Capa da VIP" className="w-full h-full" width={150} height={150} fetchPriority={"auto"} quality={100}/>
+        </div>
+        <div className="flex space-x-10 flex-auto justify-center items-center">
+            <a>Inicio</a>
+            <a>Inicio</a>
+            <a>Inicio</a>
+            <a>Inicio</a>
+        </div>
+        <div className="flex items-center">
+            <input className="border-b outline-none px-2 mr-7 border-green-500 placeholder:text-green-500" placeholder="Pesquisar"/>
+            <span className="absolute right-10 text-green-500">
+                <FaSearch/>
+            </span>
+        </div>
     </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,13 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-      <>
-        <Navbar/>
-        
-      </>
-  )
+    <>
+      <Navbar />
+      <div className="w-full h-full min-w-screen min-h-screen px-20 py-10 text-black flex justify-center" style={{backgroundImage: "url(/Capa.j)"}}>
+        <div className="h-[50vh] bg-[#16a34a87] p-4 w-full">
+          <h1 className="text-2xl font-bold text-white text-center">VIP </h1>
+        </div>
+      </div>
+    </>
+  );
 }
