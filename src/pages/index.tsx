7a1afdae1ceed49578bde6e8228b2845dba47d5e
Rodrigo Junior <165495511+rodrigojr09@users.noreply.github.com>
2025-03-19 16:57:06 -0300

    feat *

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -183,7 +183,7 @@ export default function Home() {
 				id=""
 			>
 				<iframe
-					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3713.4390441650357!2d-47.01293208887835!3d-21.451283087397723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b7b8467a688949%3A0x5e8ab75e26ff24e!2sVIP%20Engenharia%20de%20Seguran%C3%A7a%20e%20Medicina%20do%20Trabalho!5e0!3m2!1spt-BR!2sbr!4v1742346532196!5m2!1spt-BR!2sbr"
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1856.507541664524!2d-47.00499061411462!3d-21.467924797464494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b7b8467a688949%3A0x5e8ab75e26ff24e!2sVIP%20Engenharia%20de%20Seguran%C3%A7a%20e%20Medicina%20do%20Trabalho!5e0!3m2!1spt-BR!2sbr!4v1742414176547!5m2!1spt-BR!2sbr"
 					className="border-0 min-w-[600px] min-h-[500px] w-full h-full"
 					allowFullScreen
 					loading="lazy"
